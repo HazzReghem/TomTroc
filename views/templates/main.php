180dd46430203bc30b46d16a8264ba8d41b19fbc
Thibault Roussel <thibault.roussel@gmx.com>
2024-08-09 11:03:26 +0200

    header & footer added

--- views/templates/main.php
+++ views/templates/main.php
@@ -1,20 +1,36 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tom Troc</title>
-    <link rel="stylesheet" href="./css/style.css">
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Tom Troc</title>
+        <link rel="stylesheet" href="./css/style.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=League+Spartan:wght@500;600;700&family=Manrope:wght@200..800&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+        <script src="https://kit.fontawesome.com/beda2b5283.js" crossorigin="anonymous"></script>
+    </head>
 <body>
+
 <header>
-    <h1>Bienvenue sur mon site</h1>
-    <nav>
-        <ul>
-            <li><a href="/">Accueil</a></li>
-            <li><a href="/about">À propos</a></li>
-        </ul>
-    </nav>
+    <div class="headerContainer">
+        <div class="headerLogo">
+            <img src="./css/assets/logo_header.png" alt="">
+        </div>
+
+        <div class="navContainer">
+            <nav class="mainNav">
+                <li>Accueil</li>
+                <li>Nos livres à l'échange</li>
+            </nav>
+
+            <nav class="userNav">
+                <li class="userInfo"><i class="fa-regular fa-comment"></i>Messagerie</li>
+                <li class="userInfo"><i class="fa-regular fa-user"></i>Mon compte</li>
+                <li>Connexion</li>
+            </nav>
+        </div>
+    </div>
 </header>
 
 <main>
@@ -22,7 +38,14 @@
 </main>
 
 <footer>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores quam, eveniet sint veniam vero optio, impedit nesciunt porro laboriosam aspernatur suscipit. Asperiores voluptas maiores ullam vel numquam omnis laboriosam nisi?</p>
+   <div class="footer">
+        <ul>
+            <li>Politique de confidentialité</li>
+            <li>Mentions légales</li>
+            <li>Tom Troc©</li>
+            <li><img src="./css/assets/TT_logo.png" alt="Logo abréviation Tom Troc"></li>
+        </ul>
+   </div>
 </footer>
 </body>
 </html>
