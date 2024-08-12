838fb4ce9559cd45cd2fb9b029b62ab3f7b34fb9
Thibault Roussel <thibault.roussel@gmx.com>
2024-08-12 12:28:23 +0200

    main illustration's legend

--- views/templates/home.php
+++ views/templates/home.php
@@ -6,6 +6,9 @@
         <p>Donnez une nouvelle vie à vos livres en les échangeant avec d'autres amoureux de la lecture. Nous croyons en la magie du partage de connaissances et d'histoires à travers les livres.</p>
         <a href="#" class="submit">Découvrir</a>
     </div>
-    <img src="./css/assets/library.webp" alt="photo d'un homme avec des centaines de libres">
+    <div class="discoverIllustation">
+        <img src="./css/assets/library.webp" alt="photo d'un homme avec des centaines de libres">
+        <p>Hamza</p>
+    </div>
 </section>
 
