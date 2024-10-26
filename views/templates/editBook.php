cfca201d85f817eeb7580f3eca78d93c5bcb00e2
Thibault Roussel <thibault.roussel@gmx.com>
2024-10-26 14:15:27 +0200

    Edit Book Info uses entitie

--- views/templates/editBook.php
+++ views/templates/editBook.php
@@ -4,7 +4,6 @@
 
     <div class="editHeader">
         <a class="filAriane" href="index.php?action=account"><i class="fa-solid fa-arrow-left"></i>retour</a>
-
         <h2 id="editBook-section-title">Modifier les informations</h2>
     </div>
 
@@ -14,41 +13,35 @@
 
             <p>Photo</p>
             <?php
-                $picturePath = "assets/images/" . $book['image'];
+                $picturePath = "assets/images/" . $book->getImage();
                 echo '<img src="' . $picturePath . '" alt="Photo de couverture" class="bookPicture">';                
             ?>
 
             <form action="index.php?action=updateBookImage" method="post" enctype="multipart/form-data">
-    
-                <input type="hidden" name="book_id" value="<?= htmlspecialchars($book['id']) ?>">
+                <input type="hidden" name="book_id" value="<?= htmlspecialchars($book->getId()) ?>">
                 <label for="book_picture" class="custom-file-upload">Modifier la photo</label>
                 <input type="file" id="book_picture" name="book_picture" onchange="this.form.submit()" accept="image/*">
-
             </form>
 
         </div>
 
         <div class="editForm">
             <form action="index.php?action=updateBookDetails" method="post" enctype="multipart/form-data">
-            
-                <input type="hidden" name="book_id" value="<?= htmlspecialchars($book['id']) ?>">
+                <input type="hidden" name="book_id" value="<?= htmlspecialchars($book->getId()) ?>">
 
                 <label for="title">Titre</label>
-                <input type="text" id="title" name="title" value="<?= htmlspecialchars($book['title']) ?>" required>
+                <input type="text" id="title" name="title" value="<?= htmlspecialchars($book->getTitle()) ?>" required>
 
                 <label for="author">Auteur</label>
-                <input type="text" id="author" name="author" value="<?= htmlspecialchars($book['author']) ?>" required>
+                <input type="text" id="author" name="author" value="<?= htmlspecialchars($book->getAuthor()) ?>" required>
 
                 <label for="description">Commentaire</label>
-                <textarea id="description" name="description" required><?= htmlspecialchars($book['description']) ?></textarea>
-
-                <label for="description">Auteur</label>
-                <input type="text" id="description" name="description" value="<?= htmlspecialchars($book['description']) ?>" required>
+                <textarea id="description" name="description" required><?= htmlspecialchars($book->getDescription()) ?></textarea>
 
                 <label for="availability_status">Disponibilité</label>
                 <select id="availability_status" name="availability_status" required>
-                    <option value="disponible" <?= $book['availability_status'] == 'Disponible' ? 'selected' : '' ?>>Disponible</option>
-                    <option value="Pas disponible" <?= $book['availability_status'] == 'Pas disponible' ? 'selected' : '' ?>>Pas disponible</option>
+                    <option value="disponible" <?= $book->getAvailabilityStatus() == 'Disponible' ? 'selected' : '' ?>>Disponible</option>
+                    <option value="Pas disponible" <?= $book->getAvailabilityStatus() == 'Pas disponible' ? 'selected' : '' ?>>Pas disponible</option>
                 </select>
 
                 <button type="submit" class="submit">Valider</button>
